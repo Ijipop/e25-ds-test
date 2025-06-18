6aa61b0cfac864689277dabcffe13d7b5fc2aed1
Ijipop <jfthebeanz@hotmail.com>
2025-06-18 09:51:17 -0400

    Lier le script2

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const saluer = (nom) => {
   console.log(`Bonjour ${nom}`);
 };
+const saluerAvecSurnom = (nom, surnom) => {
+  console.log(`Bonjour ${nom}, aussi connu sous le nom de ${surnom}`);
+};
 
-saluer("Jf");
+saluer("John"); // Output: Bonjour John
+saluerAvecSurnom("John", "Johnny"); // Output: Bonjour John, aussi connu sous le nom de Johnny
